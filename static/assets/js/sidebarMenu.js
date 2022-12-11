22e0b2004c542de816cd5c020bae39fcf1289a26
Rasiol Isufaj <rasiolisufaj47@gmail.com>
2022-12-11 20:29:26 +0100

    locations sidebar

--- static/assets/js/sidebarMenu.js
+++ static/assets/js/sidebarMenu.js
@@ -1,6 +1,21 @@
+// Sidebar Menu
 $(document).ready(function(){
      $('.sub-btn').click(function(){
        $(this).next('.sub-menu').slideToggle();
        $(this).find('.dropdown').toggleClass('rotate');
      });
    });
+
+// Sidebar Locations Link
+const sidebarElement = document.getElementById('sidebar');
+const locationsLinkElement = document.querySelector('.locations-link');
+
+function locationSidebar(sidebar){
+  sidebar.innerHTML = `
+  <div id="sidebar" class ="sx">
+    <a href="#" class="locations-link locations-blue">LOCATIONS</a>
+  </div>
+  `
+}
+
+locationsLinkElement.addEventListener('click', () => locationSidebar(sidebarElement));
