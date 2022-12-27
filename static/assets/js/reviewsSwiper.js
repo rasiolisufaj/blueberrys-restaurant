7a7fc08a8bb91c516fb6356c14fd8393ef0ab55b
Rasiol Isufaj <rasiolisufaj47@gmail.com>
2022-12-27 15:16:36 +0100

    reviews carousel

--- static/assets/js/reviewsSwiper.js
+++ static/assets/js/reviewsSwiper.js
@@ -1,12 +1,14 @@
 var swiper = new Swiper(".slide-content", {
   slidesPerView: 3,
-  spaceBetween: 30,
-  slidesPerGroup: 3,
+  spaceBetween: 25,
   loop: true,
-  loopFillGroupWithBlank: true,
+  centerSlide: 'true',
+  fade: 'true',
+  grabCursor: 'true',
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
+    dynamicBullets: true
   },
   navigation: {
     nextEl: ".swiper-button-next",
