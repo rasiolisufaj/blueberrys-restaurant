ca5b0a7f599c2dc2f8e0eeaa9ce456cc255e922d
Rasiol Isufaj <rasiolisufaj47@gmail.com>
2023-01-21 20:50:46 +0100

    login popup bug fixed

--- static/assets/js/loginPopup.js
+++ static/assets/js/loginPopup.js
@@ -6,12 +6,22 @@ const blogElement = document.getElementById('blogHome');
 const reviewsElement = document.getElementById('reviews');
 const footerElement = document.getElementById('footer');
 const loginPopupElement = document.getElementById('loginSectionPopup');
+const contactUsSectionEl = document.getElementById('contactUsSection');
+const lastSectionOfContactEl = document.getElementById('lastSectionOfContactUs');
+const urlParts = window.location.pathname.split('/');
+const url = urlParts[urlParts.length - 1];
+console.log(url);
 
 function loginPopup(){
-  homeElement.classList.toggle('hidden');
-  aboutElement.classList.toggle('hidden');
-  blogElement.classList.toggle('hidden');
-  reviewsElement.classList.toggle('hidden');
+  if(url === 'index.html'){
+    homeElement.classList.toggle('hidden');
+    aboutElement.classList.toggle('hidden');
+    blogElement.classList.toggle('hidden');
+    reviewsElement.classList.toggle('hidden');
+  } else if(url === 'contact_us.html'){
+    lastSectionOfContactEl.classList.toggle('hidden');
+    contactUsSectionEl.classList.toggle('hidden');
+  }
   footerElement.classList.toggle('hidden');
   loginPopupElement.classList.toggle('showUp');
 }
