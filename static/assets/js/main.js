ed04b3f7116dd537bd01e44c484f39eee38b119f
Rasiol Isufaj <rasiolisufaj47@gmail.com>
2023-01-25 13:47:04 +0100

    slogan effect

--- static/assets/js/main.js
+++ static/assets/js/main.js
@@ -1,9 +1,7 @@
 (function () {
   "use strict";
 
-  /**
-   * Animation on scroll
-   */
+  /* Animation on scroll */
   window.addEventListener("load", () => {
     AOS.init({
       duration: 1000,
@@ -12,5 +10,19 @@
       mirror: false,
     });
   });
+
+  /* Home type effect */
+  const typed = document.querySelector(".typed");
+  if (typed) {
+    let typed_strings = typed.getAttribute("data-typed-items");
+    typed_strings = typed_strings.split(",");
+    new Typed(".typed", {
+      strings: typed_strings,
+      loop: true,
+      typeSpeed: 100,
+      backSpeed: 50,
+      backDelay: 2000,
+    });
+  }
 
 })();
