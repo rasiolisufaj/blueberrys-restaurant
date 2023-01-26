6945815ac64dd2947d13bbce00886ee664f48c0e
Rasiol Isufaj <rasiolisufaj47@gmail.com>
2023-01-26 13:19:05 +0100

    read more v.1

--- static/assets/js/readMore.js
+++ static/assets/js/readMore.js
@@ -1,16 +1,18 @@
 (function () {
   "use strict";
-
+  const dots = document.getElementById("dots");
+  const moreText = document.getElementById("more");
+  const moreParagraphs = document.querySelectorAll('#more p.aboutUsPara');
+  console.log(moreParagraphs);
+  const btnText = document.getElementById("readMoreText");
+  const aboutUsDescription = document.getElementById("aboutUsDescription");
+  const upicon = document.getElementById("aboutUsArrowUp");
+  const downicon = document.getElementById("aboutUsArrowDown");
+  const aboutContainer = document.getElementById("about");
   let readMoreButton = document.getElementById("readMoreButton");
+
   readMoreButton.addEventListener("click", function (e) {
     e.preventDefault();
-    var dots = document.getElementById("dots");
-    var moreText = document.getElementById("more");
-    var btnText = document.getElementById("readMoreText");
-    var aboutUsDescription = document.getElementById("aboutUsDescription");
-    var upicon = document.getElementById("aboutUsArrowUp");
-    var downicon = document.getElementById("aboutUsArrowDown");
-    var aboutContainer = document.getElementById("about");
     // let height = window.innerHeight ;
     if (screen.width >= 800 || screen.width <= 436) {
       if (dots.style.display === "none") {
@@ -20,15 +22,17 @@
         aboutUsDescription.style.height = "343px";
         downicon.style.display = "inline";
         upicon.style.display = "none";
-        aboutUsDescription.style.transition = "height 1s ease";
+        // aboutUsDescription.style.transition = "height 1s ease";
       } else {
         dots.style.display = "none";
         btnText.innerHTML = "Read less";
-        moreText.style.display = "inline";
         aboutUsDescription.style.height = "690px";
         downicon.style.display = "none";
         upicon.style.display = "inline";
-        aboutUsDescription.style.transition = "height 1s ease";
+        aboutUsDescription.style.transition = "height 0.5s ease-in-out";
+        setTimeout(() => {
+          moreText.style.display = 'block';
+        }, 500);
       }
     }
   });
