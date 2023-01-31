a17e401fe7a5a30604bb495c1f84bc4d80391c63
Rasiol Isufaj <rasiolisufaj47@gmail.com>
2023-01-31 16:23:05 +0100

    Blog v1

---
+++ static/assets/js/blog.js
@@ -0,0 +1,7 @@
+const post1Benefits = document.getElementById('post1-benefits');
+const post1Ingredients = document.getElementById('post1-ingredients');
+const post1Tutorial = document.getElementById('post1-tutorial');
+const post1BenefitsElement = document.getElementById('postBenefits');
+const post1IngredientsElement = document.getElementById('postIngredients');
+const post1TutorialElement = document.getElementById('postTutorial');
+
