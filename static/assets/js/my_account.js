e7ba23daf0e79516f16f4c37162923f40a3d3501
Rasiol Isufaj <rasiolisufaj47@gmail.com>
2023-02-10 10:58:03 +0100

    Css Changes

--- static/assets/js/my_account.js
+++ static/assets/js/my_account.js
@@ -36,47 +36,47 @@ myAccountLink.addEventListener('click', (e) => {
   changePasswordElement.classList.add('hidden');
 });
 
-orderHistoryLink.addEventListener('click', (e) => {
-  e.preventDefault();
-  // myAccountLink.classList.remove('activeLink');
-  // orderHistoryLink.classList.add('activeLink');
-  // myWishlistLink.classList.remove('activeLink');
-  // paymentDetailsLink.classList.remove('activeLink');
-  // changePasswordLink.classList.remove('activeLink');
-  // myAccountElement.classList.add('hidden');
-  // orderHistoryElement.classList.remove('hidden');
-  // myWishlistElement.classList.add('hidden');
-  // paymentDetailsElement.classList.add('hidden');
-  // changePasswordElement.classList.add('hidden');
-});
+// orderHistoryLink.addEventListener('click', (e) => {
+//   e.preventDefault();
+//   myAccountLink.classList.remove('activeLink');
+//   orderHistoryLink.classList.add('activeLink');
+//   myWishlistLink.classList.remove('activeLink');
+//   paymentDetailsLink.classList.remove('activeLink');
+//   changePasswordLink.classList.remove('activeLink');
+//   myAccountElement.classList.add('hidden');
+//   orderHistoryElement.classList.remove('hidden');
+//   myWishlistElement.classList.add('hidden');
+//   paymentDetailsElement.classList.add('hidden');
+//   changePasswordElement.classList.add('hidden');
+// });
 
-myWishlistLink.addEventListener('click', (e) => {
-  e.preventDefault();
-  // myAccountLink.classList.remove('activeLink');
-  // orderHistoryLink.classList.remove('activeLink');
-  // myWishlistLink.classList.add('activeLink');
-  // paymentDetailsLink.classList.remove('activeLink');
-  // changePasswordLink.classList.remove('activeLink');
-  // myAccountElement.classList.add('hidden');
-  // orderHistoryElement.classList.add('hidden');
-  // myWishlistElement.classList.remove('hidden');
-  // paymentDetailsElement.classList.add('hidden');
-  // changePasswordElement.classList.add('hidden');
-});
+// myWishlistLink.addEventListener('click', (e) => {
+//   e.preventDefault();
+//   myAccountLink.classList.remove('activeLink');
+//   orderHistoryLink.classList.remove('activeLink');
+//   myWishlistLink.classList.add('activeLink');
+//   paymentDetailsLink.classList.remove('activeLink');
+//   changePasswordLink.classList.remove('activeLink');
+//   myAccountElement.classList.add('hidden');
+//   orderHistoryElement.classList.add('hidden');
+//   myWishlistElement.classList.remove('hidden');
+//   paymentDetailsElement.classList.add('hidden');
+//   changePasswordElement.classList.add('hidden');
+// });
 
-paymentDetailsLink.addEventListener('click', (e) => {
-  e.preventDefault();
-  // myAccountLink.classList.remove('activeLink');
-  // orderHistoryLink.classList.remove('activeLink');
-  // myWishlistLink.classList.remove('activeLink');
-  // paymentDetailsLink.classList.add('activeLink');
-  // changePasswordLink.classList.remove('activeLink');
-  // myAccountElement.classList.add('hidden');
-  // orderHistoryElement.classList.add('hidden');
-  // myWishlistElement.classList.add('hidden');
-  // paymentDetailsElement.classList.remove('hidden');
-  // changePasswordElement.classList.add('hidden');
-});
+// paymentDetailsLink.addEventListener('click', (e) => {
+//   e.preventDefault();
+//   myAccountLink.classList.remove('activeLink');
+//   orderHistoryLink.classList.remove('activeLink');
+//   myWishlistLink.classList.remove('activeLink');
+//   paymentDetailsLink.classList.add('activeLink');
+//   changePasswordLink.classList.remove('activeLink');
+//   myAccountElement.classList.add('hidden');
+//   orderHistoryElement.classList.add('hidden');
+//   myWishlistElement.classList.add('hidden');
+//   paymentDetailsElement.classList.remove('hidden');
+//   changePasswordElement.classList.add('hidden');
+// });
 
 changePasswordLink.addEventListener('click', (e) => {
   e.preventDefault();
